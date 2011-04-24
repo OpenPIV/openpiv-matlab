1d6b14e34133a5fd54d8408575c671903c7a9114
Alex Liberzon <alex.liberzon@gmail.com>
2011-04-24 15:47:11 +0300

    fixed the cross_correlate_rect back to the original subtraction of the mean

--- urapiv_gui.m
+++ urapiv_gui.m
@@ -1228,16 +1228,14 @@ return
 function [c] = cross_correlate_rect(a2,b2,NfftHeight,NfftWidth)
 
 % temprorary solution
-disp('fix it')
 a2 = a2 - mean2(a2);
 b2 = b2 - mean2(b2);
-a2(a2<0) = 0;
-b2(b2<0) = 0;
 
 b2 = b2(end:-1:1,end:-1:1);
 
 ffta=fft2(single(a2),NfftHeight,NfftWidth);
 fftb=fft2(single(b2),NfftHeight,NfftWidth);
+
 c = real(ifft2(ffta.*fftb));
 return
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
