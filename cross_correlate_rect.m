058a17e92c7446e79d235a91abce607a6284451c
francescotoselli <francesco.toselli@edu.unito.it>
2016-03-01 20:49:35 +0100

    Added files via upload

--- cross_correlate_rect.m
+++ cross_correlate_rect.m
@@ -1,4 +1,4 @@
-function [c,c2] = cross_correlate_rect(a2,b2,NfftHeight,NfftWidth)
+function [c] = cross_correlate_rect(a2,b2,NfftHeight,NfftWidth)
 
 % temprorary solution
 a2 = a2 - mean2(a2);
@@ -13,11 +13,8 @@ c = real(ifft2(ffta.*fftb));
 
 % another option not implemented yet, but b2 shall be larger than a2
 % see >> help normxcorr2
-a3=a2(2:end-1,2:end-1); %taglio bordi di a2
-b2 = b2(end:-1:1,end:-1:1); %raddrizzo l'immagine
-c2 = normxcorr2(a3,b2); %calcolo correlazione normalizzata
 % c = normxcorr2(b2,a2);
 
 c(c<0) = 0;
-c2(c2<0) = 0;
+
 return
