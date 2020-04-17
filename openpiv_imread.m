74a3fa5fac8a19771114117bc3d028adbce70f01
Alex Liberzon <alex.liberzon@gmail.com>
2020-04-18 00:43:22 +0300

    Revert "I found that this is a bug that I introduced by merging an old branch on Jan 12, 2020"
    This reverts commit b4537c7277b8dcc872e48fa155ce00458bb09869.

--- openpiv_imread.m
+++ openpiv_imread.m
@@ -1,15 +1,15 @@
-function im = openpiv_imread(handles,filenum)
+function im = openpiv_imread(image_name)
 % openpiv_imread encapsulates all the image reading functions
 % that can be imread for 'jpg','bmp', etc. or 'tiffread2' for TIFF
 % images from Insight (tm)
 % Usage:
-% >>  im = openpiv_imread(handles,file_number);
+% >>  im = openpiv_imread(directory, image_name);
 % >>  imshow(im);
 
 try
-    im = imread(fullfile(handles.path,handles.files{filenum}));
+    im = imread(image_name);
 catch
-    tmp = tiffread2(fullfile(handles.path,handles.files{filenum}));
+    tmp = tiffread2(image_name);
     im = im2double(tmp.data);
 end
 
@@ -17,11 +17,11 @@ if ndims(im) == 3
     im = rgb2gray(im);
 end
 
-% Custom pre-processing of images, default = 'imadjust'
-preprocess = get(handles.checkbox_preprocess,'Value');
-if preprocess
-    prepfun = str2func(handles.preprocess);
-else
-    prepfun = @(x)imadjust(x); % default is to stretch the image
-end
-im = prepfun(im);
+% % Custom pre-processing of images, default = 'imadjust'
+% preprocess = get(handles.checkbox_preprocess,'Value');
+% if preprocess
+%     prepfun = str2func(handles.preprocess);
+% else
+%     prepfun = @(x)imadjust(x); % default is to stretch the image
+% end
+% im = prepfun(im);
