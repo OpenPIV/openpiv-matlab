b4537c7277b8dcc872e48fa155ce00458bb09869
Alex Liberzon <alex.liberzon@gmail.com>
2020-04-18 00:26:12 +0300

    I found that this is a bug that I introduced by merging an old branch on Jan 12, 2020

--- read_pair_of_images_rect.m
+++ read_pair_of_images_rect.m
@@ -1,4 +1,5 @@
-function [A,B,A1,B1,origin] = read_pair_of_images_rect(image1,image2,cropvec,ittWidth,ittHeight,ovlapHor,ovlapVer)
+function [A,B,A1,B1,origin] = read_pair_of_images_rect(handles,fileind,jump,cropvec,ittWidth,ittHeight,ovlapHor,ovlapVer)
+
 % READ_PAIR_OF_IMAGES - reads two images (image1,image2) as BMP files
 % and crops them according to 'cropvec'
 % Inputs:
@@ -23,8 +24,8 @@ origin = [0,0];
  %image2 = fullfile(handles.path,handles.files{fileind+jump});
 
 % try
-    A = openpiv_imread(image1);
-    B = openpiv_imread(image2);
+    A = openpiv_imread(handles,fileind); % ,'tif'); 8 June 2005, no extension is necessary
+    B = openpiv_imread(handles,fileind+jump); % ,'tif'); % more flexibility for different image formats
 % catch
 %     warning off
 %     tmp = tiffread2(image1);
@@ -43,7 +44,7 @@ origin = [0,0];
 
 
 % Crop the images to the desired size and
-% cut the last couple of pixels, so we'll get the
+% cut the last couple of pixels, so we will get the
 % integer number of interrogation areas
 %
 %       ---- t ---
